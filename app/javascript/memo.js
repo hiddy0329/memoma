e41da156e7ffb13cd398003594ae46ef8dc5625d
hiddy0329 <hiddy19970329@gmail.com>
2022-04-22 20:45:23 +0900

    memo.js更新

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,44 @@
-function post() {
-  console.log("イベント発火");
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.memo}
+      </div>
+    </div>`;
+  return html;
+};
+
+function post (){
+  const submit = document.getElementById("submit");
+  
+  submit.addEventListener("mouseover", function(){
+    const memo = document.getElementById("memo");
+    memo.setAttribute("style", "display: inline;");
+  });
+
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("memo");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
+  });
 };
 
 window.addEventListener('load', post);
